4c35eb497e708ecf76e7c69501e12eb504fe6c03
Oussema Sferi <oussema.sferi@gmail.com>
2022-12-12 01:03:50 +0100

    easymde customized

--- app/Ressources/js/markdown.js
+++ app/Ressources/js/markdown.js
@@ -1,4 +1,4 @@
 var easyMDE = new EasyMDE({
     element: document.getElementById('postDistribution'),
-    toolbar: ['bold', 'italic', 'heading', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', '|', 'preview', 'guide']
+    toolbar: ['bold', 'italic', 'heading','heading-1', 'heading-2', 'heading-3', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', '|', 'preview']
 });
