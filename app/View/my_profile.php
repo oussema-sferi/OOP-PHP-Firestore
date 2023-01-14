cdb6039dcfaa219ba4cdaf2bd123372820d3a77c
oussema sferi <oussema.sferi@gmail.com>
2023-01-14 18:35:21 +0100

    my profile page created

---
+++ app/View/my_profile.php
@@ -0,0 +1,193 @@
+<?php
+namespace App\View;
+use App\Model\Firestore_honeydoo;
+require_once "../../vendor/autoload.php";
+
+if(!isset($_SESSION["user"]))
+{
+    header("Location: login.php");
+} else {
+    if(isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] == "ROLE_ADMIN")
+    {
+        header("Location: users_list.php");
+    }
+}
+/*$database = new Firestore_honeydoo();
+$userClients = $database->fetchUserClients($_SESSION["user"]["realtor_id"]);*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Honeydoo" />
+    <meta name="author" content="Honeydoo" />
+    <title>My Profile</title>
+    <link href="../Ressources/css/styles.css" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="../Ressources/assets/img/favicon.png" />
+    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body class="nav-fixed">
+<nav class="topnav navbar navbar-expand shadow justify-content-between justify-content-sm-start navbar-light bg-white" style="background-color: #262144!important" id="sidenavAccordion">
+    <!-- Sidenav Toggle Button-->
+    <button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 me-2 ms-lg-2 me-lg-0" id="sidebarToggle" style="background-color: #00B2A0!important;"><i data-feather="menu"></i></button>
+    <!-- Navbar Brand-->
+    <!--<a class="navbar-brand pe-3 ps-4 ps-lg-2" href="#">Honeydoo</a>-->
+    <img class="navbar-brand pe-3 ps-4 ps-lg-2" src="../Ressources/assets/img/HoneyDoo-logo.png">
+    <!-- Navbar Items-->
+    <ul class="navbar-nav align-items-center ms-auto">
+        <!-- User Dropdown-->
+        <li class="nav-item dropdown no-caret dropdown-user me-3 me-lg-4">
+            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="../Ressources/assets/img/illustrations/profiles/profile-4.png" /></a>
+            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
+                <h6 class="dropdown-header d-flex align-items-center">
+                    <img class="dropdown-user-img" src="../Ressources/assets/img/illustrations/profiles/profile-4.png" />
+                    <div class="dropdown-user-details">
+                        <div class="dropdown-user-details-name"><?=$_SESSION["user"]["realtor_title"]?></div>
+                        <div class="dropdown-user-details-email"><?=$_SESSION["user"]["email"]?></div>
+                    </div>
+                </h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="<?php echo 'my_profile.php' ?>">
+                    <div class="dropdown-item-icon"><i data-feather="user"></i></div>
+                    My Profile
+                </a>
+                <a class="dropdown-item" href="<?php echo 'logout.php' ?>">
+                    <div class="dropdown-item-icon"><i data-feather="log-out"></i></div>
+                    Logout
+                </a>
+            </div>
+        </li>
+    </ul>
+</nav>
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sidenav shadow-right sidenav-light">
+            <div class="sidenav-menu">
+                <div class="nav accordion" id="accordionSidenav">
+                    <!-- Sidenav Heading (Custom)-->
+                    <div class="sidenav-menu-heading">Dashboard</div>
+                    <!-- Sidenav Link (Blog Posts)-->
+                    <a class="nav-link" href="<?='blog_posts.php'?>">
+                        <div class="nav-link-icon"><i data-feather="list"></i></div>
+                        My Stories
+                    </a>
+                    <!-- Sidenav Link (Pro Services)-->
+                    <a class="nav-link" href="<?='pro_services.php'?>">
+                        <div class="nav-link-icon"><i data-feather="list"></i></div>
+                        My Pro Services
+                    </a>
+                    <!-- Sidenav Link (Client)-->
+                    <a class="nav-link" href="<?='clients_list.php'?>">
+                        <div class="nav-link-icon"><i data-feather="list"></i></div>
+                        Clients List
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div id="layoutSidenav_content">
+        <main>
+            <header class="page-header page-header-compact page-header-light border-bottom bg-white mb-4">
+                <div class="container-xl px-4">
+                    <div class="page-header-content">
+                        <div class="row align-items-center justify-content-between pt-3">
+                            <div class="col-auto mb-3">
+                                <h1 class="page-header-title">
+                                    <div class="page-header-icon"><i data-feather="user"></i></div>
+                                    My Profile
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- Main page content-->
+            <div class="container-xl px-4 mt-4">
+                <div class="row">
+                    <div class="col-xl-4">
+                        <!-- Profile picture card-->
+                        <div class="card mb-4 mb-xl-0">
+                            <div class="card-header text-center">Profile Picture</div>
+                            <div class="card-body text-center">
+                                <!-- Profile picture image-->
+                                <img class="img-account-profile rounded-circle mb-2" src="assets/img/illustrations/profiles/profile-1.png" alt="" />
+                                <!-- Profile picture help block-->
+                                <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                                <!-- Profile picture upload button-->
+                                <button class="btn btn-primary" type="button">Upload new image</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-8">
+                        <!-- Account details card-->
+                        <div class="card mb-4">
+                            <div class="card-header text-center">Account Details</div>
+                            <div class="card-body">
+                                <form>
+                                    <div class="row gx-3 mb-3">
+                                        <div class="col-md-6">
+                                            <label class="small mb-1" for="inputFirstName">Full name</label>
+                                            <input class="form-control" id="fullName" name="fullName" placeholder="Enter your full name..." type="text" value="" disabled/>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
+                                            <input class="form-control" id="emailAddress" name="emailAddress" placeholder="Enter your email address..." type="email" value="" disabled/>
+                                        </div>
+                                    </div>
+
+                                    <div class="row gx-3 mb-3">
+
+                                        <div class="col-md-6">
+                                            <label class="small mb-1" for="inputLastName">Phone Number</label>
+                                            <input class="form-control" id="phoneNumber" name="phoneNumber" placeholder="Enter your phone number..." type="text" value="" disabled/>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <label class="small mb-1" for="inputOrgName">Company name</label>
+                                            <input class="form-control" id="companyName" name="companyName" placeholder="Enter your company name..." type="text" value="" disabled/>
+                                        </div>
+                                    </div>
+                                    <!-- Save changes button-->
+                                    <div class="text-center">
+                                        <button class="btn btn-primary" id="editButton" type="button">Edit</button>
+                                        <button class="btn btn-success" id="saveButton" type="button" style="display: none">Save Changes</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+        <footer class="footer-admin mt-auto footer-light">
+            <div class="container-xl px-4">
+                <div class="row">
+                    <div class="small text-center">Copyright &copy; Honeydoo</div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@5" type="text/javascript"></script>
+<script src="../Ressources/js/datatables/datatables-simple-demo.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="../Ressources/js/scripts.js"></script>
+<script>
+    $( document ).ready(function() {
+        $("#editButton").click(function () {
+            $(this).hide()
+            $("#saveButton").show()
+            $("input").prop('disabled', false);
+        })
+    });
+</script>
+</body>
+</html>
