da9afcbbf9a8d651175e9d659bbb0cc085892751
oussema sferi <oussema.sferi@gmail.com>
2023-01-16 02:59:28 +0100

    smtp implemented and sending invit emails ok

--- app/Controller/send_email_invitation_action.php
+++ app/Controller/send_email_invitation_action.php
@@ -5,23 +5,19 @@ use App\Model\Firestore_honeydoo;
 use App\Service\MailerService;
 
 if(!isset($_SESSION["user"]))
+{
+    header("Location: login.php");
+} else {
+    if(isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] == "ROLE_ADMIN")
     {
-        header("Location: login.php");
-    } else {
-        if(isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] == "ROLE_ADMIN")
-        {
-            header("Location: ../View/users_list.php");
-        }
+        header("Location: ../View/users_list.php");
     }
-
-    $database = new Firestore_honeydoo();
-    $email = $database->fetchEmailContent();
-    $emailContent = $email["content"];
-/*var_dump($emailContent);
-die();*/
-    $mailer = new MailerService();
-    $mailer->sendInvitationMail($emailContent);
-    /*var_dump(json_decode($_POST["selectedClients"]));
-    die();*/
-    header("Location: ../View/clients_list.php");
+}
+$emailAddresses = json_decode($_POST["selectedClients"]);
+$database = new Firestore_honeydoo();
+$email = $database->fetchEmailContent();
+$emailContent = $email["content"];
+$mailer = new MailerService();
+$mailer->sendInvitationMail($emailContent, $emailAddresses);
+header("Location: ../View/clients_list.php");
 
--- app/Service/MailerService.php
+++ app/Service/MailerService.php
@@ -11,20 +11,24 @@ class MailerService
         $this->mailer->isSMTP();
         $this->mailer->SMTPDebug = 2;
         $this->mailer->Debugoutput = 'error_log';
-        $this->mailer->Host = '127.0.0.1';
-        $this->mailer->Port = 1025;
-        $this->mailer->SMTPAuth = false;
-        /*$this->mailer->Username = 'amessuo@huzokujapan.v-info.info';
-        $this->mailer->Password = 'AZ123456az@';*/
+        $this->mailer->Host = 'smtp.office365.com';
+        $this->mailer->Port = 587;
+        $this->mailer->SMTPAutoTLS = true;
+        $this->mailer->SMTPAuth = true;
+        $this->mailer->Username = 'info@honeydoo.io';
+        $this->mailer->Password = 'Tmvh5282!';
     }
 
-    public function sendInvitationMail($content)
+    public function sendInvitationMail($content, $emailAddresses)
     {
-        $this->mailer->setFrom("user@honeydoo.dev", "Honeydoo");
-        $this->mailer->addReplyTo("user@honeydoo.dev", "Honeydoo");
-        $this->mailer->addAddress("oussema.sferi@gmail.com", "Oussema Sferi");
+        $this->mailer->setFrom("info@honeydoo.io", "Honeydoo");
+        $this->mailer->addReplyTo("info@honeydoo.io", "Honeydoo");
+        foreach ($emailAddresses as $emailRecipient)
+        {
+            $this->mailer->AddAddress($emailRecipient);
+        }
         $this->mailer->isHTML(true);
-        $this->mailer->Subject = "Invitation Email";
+        $this->mailer->Subject = "App Download Invitation";
         $this->mailer->Body = $content;
         if (!$this->mailer->send()) {
             return 'Mailer Error: ' . !$this->mailer->ErrorInfo;
