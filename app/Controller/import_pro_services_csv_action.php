84e0551f736f30a07796adb713a2ebd97d1c447e
oussema sferi <oussema.sferi@gmail.com>
2023-02-16 19:36:26 +0100

    push notifications fixed for import home pros from csv

--- app/Controller/import_pro_services_csv_action.php
+++ app/Controller/import_pro_services_csv_action.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 require_once "../../vendor/autoload.php";
 use App\Model\Firestore_honeydoo;
+use App\Service\HelperService;
 use DateTime;
 use Google\Cloud\Core\Timestamp;
 use PhpOffice\PhpSpreadsheet\IOFactory;
@@ -18,6 +19,7 @@ if(!isset($_SESSION["user"]))
 $path = "";
 if (isset($_POST["submit"]))
 {
+    $loggedRealtorId = $_SESSION["user"]["realtor_id"];
     $filePath =  "/app/Ressources/import_files/pro_services/" . md5(uniqid()) . $_FILES["excelHomeProsfile"]["name"];
     move_uploaded_file(
         $_FILES["excelHomeProsfile"]["tmp_name"], $_SERVER['DOCUMENT_ROOT'] . $filePath
@@ -39,7 +41,7 @@ if (isset($_POST["submit"]))
             'comments' => $row["F"],
             'my_notes' => $row["G"],
             'date' => new Timestamp(new DateTime()),
-            'realtor_id' => $_SESSION["user"]["realtor_id"]
+            'realtor_id' => $loggedRealtorId
         ];
         $allData[] = $rowData;
     }
@@ -48,6 +50,31 @@ if (isset($_POST["submit"]))
     {
         $database->createNewProService($row);
     }
+    // Here comes the push notifications
+    $helper = new HelperService();
+    $realtorLinkedPortalClients = $database->fetchUserClients($loggedRealtorId);
+    $allMobileAppClients = $database->fetchAllMobileAppClients();
+    $helper->clientCheckAndSaveSignUpDate($realtorLinkedPortalClients, $allMobileAppClients, $database);
+    $realtorLinkedMobileClientsTokens = [];
+    foreach ($realtorLinkedPortalClients as $portalClient)
+    {
+        if(isset($portalClient->notification_token) && trim($portalClient->notification_token) !== "")
+        {
+            $realtorLinkedMobileClientsTokens[] = $portalClient->notification_token;
+        }
+    }
+    $notificationParameters = [
+        "title" => "HoneyDoo Alert",
+        "body" => "Your realtor has added a new recommended home pro. Click here to learn more."
+    ];
+    $redirectUrl = "../View/pro_services.php";
+    if(count($realtorLinkedMobileClientsTokens) > 0) {
+        $helper->sendFCM($realtorLinkedMobileClientsTokens, $notificationParameters, $redirectUrl);
+    } else {
+        header("Location: ../View/pro_services.php");
+        /*var_dump("no notifications sent");
+        die();*/
+    }
 }
-header("Location: ../View/pro_services.php");
+/*header("Location: ../View/pro_services.php");*/
 
