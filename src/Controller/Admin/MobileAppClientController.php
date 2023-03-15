466438c14d89245e40ac7e1d00f90ae74e0cd009
oussema sferi <oussema.sferi@gmail.com>
2023-03-15 20:52:19 +0100

    admin - app users menu refactored 2

--- src/Controller/Admin/MobileAppClientController.php
+++ src/Controller/Admin/MobileAppClientController.php
@@ -49,22 +49,9 @@ class MobileAppClientController
     {
         $id = $params["id"];
         $data = [
-            'first_name_1' => $_POST["firstName1"],
-            'last_name_1' => $_POST["lastName1"],
-            'email_1' => $_POST["email1"],
-            'phone_1' => $_POST["phoneNumber1"],
-            'first_name_2' => $_POST["firstName2"],
-            'last_name_2' => $_POST["lastName2"],
-            'email_2' => $_POST["email2"],
-            'phone_2' => $_POST["phoneNumber2"],
-            'address_1' => $_POST["address1"],
-            'address_2' => $_POST["address2"],
-            'city' => $_POST["city"],
-            'state' => $_POST["state"],
-            'zip' => $_POST["zipCode"],
-            'home_type' => $_POST["homeType"],
-            'notes' => $_POST["notes"],
-            'realtor_id' => $_SESSION["user"]["realtor_id"]
+            'full_name' => $params["fullName"],
+            'email' => $params["email"],
+            'phone' => $params["phoneNumber"]
         ];
         $finalData = [];
         foreach ($data as $key => $value)
