7261c02d648eb804bb4532af4167853db3b449fc
oussema sferi <oussema.sferi@gmail.com>
2023-05-12 18:07:35 +0100

    show/hide realtor from mobile app - in progress

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -62,6 +62,7 @@ class RegistrationController
             'homePro_type' => "m is simply dummy text of the printing and typesetting industry",
             'realtor_comments' => "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             'is_deleted' => false,
+            'show_in_app' => false,
             'date' => new Timestamp(new DateTime()),
         ];
         $newUserDocId = $user->createNewUser($data);
